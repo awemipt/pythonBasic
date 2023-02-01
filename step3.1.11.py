e45bc9cf23ea71d0cd8cedfb458e8c5a7e6f4184
awemipt <71894842+awemipt@users.noreply.github.com>
2023-02-01 19:00:41 +0300

    01.02.2023 end of lesson 3

---
+++ step3.1.11.py
@@ -0,0 +1,2 @@
+sym1, sym2 = input().split()
+print(f"Коды: {sym1} = {ord(sym1)}, {sym2} = {ord(sym2)}")
---
+++ step3.1.8.py
@@ -0,0 +1,6 @@
+# put your python code here
+a, b = map(int, input().split())
+print("Переменная а = " + str(a)+  ", переменная b = " + str(b))
+
+
+
